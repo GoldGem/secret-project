4ef1d7466aa9975ee0066237f649aa97439ae704
GoldGem <mishachemo@gmail.com>
2020-03-12 01:45:28 +0300

    upd

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {UsersService} from '../../shared/services/users.service';
 import {User} from '../../shared/models/user.model';
+import {Message} from '../../shared/models/message.model';
 
 @Component({
   selector: 'wfm-login',
@@ -11,13 +12,20 @@ import {User} from '../../shared/models/user.model';
 export class LoginComponent implements OnInit {
 
   form: FormGroup;
+  message: Message;
 
   constructor(
-    private userService: UsersService
+    private usersService: UsersService
   ) {}
 
-
+  private showMessage(text: string, type: string = 'danger') {
+    this.message = new Message(type, text);
+    window.setTimeout(() => {
+      this.message.text = '';
+    }, 5000);
+  }
   ngOnInit() {
+    this.message = new Message('danger', '');
     this.form = new FormGroup({
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
@@ -27,16 +35,16 @@ export class LoginComponent implements OnInit {
   onSubmit() {
     const formData = this.form.value;
 
-    this.userService.getUserByEmail(formData.email)
+    this.usersService.getUserByEmail(formData.email)
       .subscribe((user: User) => {
         if (user) {
           if (user.password === formData.password) {
 
           } else {
-            alert('Password is incorrect');
+            this.showMessage('Password is incorrect');
           }
         } else {
-            alert('User not found');
+            this.showMessage('User not found');
         }
       });
   }
